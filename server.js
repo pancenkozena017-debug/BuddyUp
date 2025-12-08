4d3e835562f86771ad90f9dc9d19f356feba8331
Малий <oleggludyn09@gmail.com>
2025-12-08 16:30:38 +0200

    server bags

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const path = require('path');
 
 const app = express();
 
+// Віддаємо всі статичні файли з поточної папки
 app.use(express.static(__dirname));
+
+// Для будь-якого GET запиту віддаємо index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
