f06f28b7f93faa68e58d3a3aeb2675ed693bbc61
Малий <oleggludyn09@gmail.com>
2025-12-08 16:32:35 +0200

    error

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 // Віддаємо всі статичні файли з поточної папки
 app.use(express.static(__dirname));
 
-// Для будь-якого GET запиту віддаємо index.html
-app.get('*', (req, res) => {
+// Для будь-якого GET запиту, який не збігається зі статичними файлами
+app.all(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
