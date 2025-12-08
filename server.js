b833672a8d9c4865384853a32a8d55811fce724f
Малий <oleggludyn09@gmail.com>
2025-12-08 16:42:02 +0200

    added

--- server.js
+++ server.js
@@ -9,5 +9,10 @@ const frontendPath = path.join(__dirname, 'buddy_up');
 // Віддаємо всі статичні файли з buddy_up
 app.use(express.static(frontendPath));
 
+// Корінь "/" віддає index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index/index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
